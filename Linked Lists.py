3df6c1e9d5937dddf03d8ffa743d78dd9a0c00a1
Vinay Baliyan <63986065+baliyanvinay@users.noreply.github.com>
2020-08-19 11:48:01 +0530

    Functions added to add/delete node  at head

--- Linked Lists.py
+++ Linked Lists.py
@@ -53,13 +53,59 @@ class Linked_list():
         while(print_head): # means print_head is not None
             print(print_head.data)
             print_head=print_head.next_node
+            
+    def add_node_head(self, data):
+        # to insert a new node at the begining of linked_list or at head position
+        new_node=Node(data)
+        new_node.next_node=self.head
+        self.head=new_node
+        
+    def remove_node_head(self):
+        # to remove node from head and make second element head
+        self.head=self.head.next_node
+        
+    def insert_node(self, data, loc):
+        # insert node at a given position in linked_list, i.e., loc
+        new_node=Node(data)
+        
+        # step1: getting index or head to the loc 
+        index, loc_head=0, self.head
+        while(index!=loc):
+            loc_head=loc_head.next_node
+            index+=1
+        # step2: insert the new_node at loc_head; save loc_head.next_node in new_node.next_node
+        new_node.next_node=loc_head.next_node
+        loc_head.next_node=new_node
+        
+    def delete_node(self, loc):
+        # remove node from a specific location
+        # step1: reaching the location
+        index, loc_head=0, self.head
+        while(index<loc):
+            loc_head=loc_head.next_node
+            index+=1
+            
+        # step2: remove the element by changing the next_node of current location head
+        temp=loc_head.next_node.next_node
+        loc_head.next_node=None
+        loc_head=temp
+        
+        
 #_________________________________________________________________________________________________#
 
 # main program
 L1=Linked_list() # instance of linked list
 L1.add_node(12)
 L1.add_node(15)
+L1.add_node_head(10)
+L1.add_node_head(5)
 
 print(L1.display_linked_list())
-L1.remove_node()
+L1.remove_node_head()
+print(L1.display_linked_list())
+
+L1.insert_node(14,1)
+print(L1.display_linked_list())
+
+L1.delete_node(0)
 print(L1.display_linked_list())
