c5838b892372ce1ece480d97de9a3e64babed828
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-08-28 22:26:10 +0530

    Binary Tree using recursion

--- Binary Tree.py
+++ Binary Tree.py
@@ -1,35 +1,50 @@
 # Implementation of Binary Tree :: 22-Aug-2020
-from binarytree import tree
-
 class Node(object):
-    def __init__(self, data): # a node in BT would have left and right child
-        self.data=data
-        self.left=None
-        self.right=None
+    def __init__(self, value): # a node in BT would have left and right child
+        self.value=value
+        self.left_child, self.right_child=None, None
         
 class BinaryTree(object):
     def __init__(self):
-        self.root=None # root will hold the topmost root node
-    
-    def add_node(self, data):
-        new_node=Node(data)
-        if not self.root:
-            self.root=new_node
+        self.root=None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root=Node(value)
         else:
-            # in else we have to check where the new_node needs to be inserted
-            temp_root=self.root # for traversing
-            while(temp_root):
-                if new_node.data<temp_root.data: 
-                    # if new node is smaller than root node, then left has to be checked
-                    temp_root=temp_root.left
-                elif new_node.data>temp_root.data:
-                    # when new node is greater than root node, then right has to be checked
-                    temp_root=temp_root.left
-                else:
-                    pass # means node already exists and no need to add the node
-            # temp_root will now point to an empty left or right, we can add a node here
-            temp_root=new_node
+            self._insert(value, self.root) # private insert function
     
-    def display_node(self):
-        pass
-        
+    def _insert(self, value, curr_node):
+        if value < curr_node.value:
+            if curr_node.left_child is None:
+                curr_node.left_child=Node(value)
+            else:
+                self._insert(value, curr_node.left_child)
+        elif value > curr_node.value:
+            if curr_node.right_child is None:
+                curr_node.right_child=Node(value)
+            else:
+                self._insert(value, curr_node.right_child)
+        else: # case where value is equal to the node
+            print("Value already in Tree")
+    
+    def print_tree(self):
+        if self.root is not None:
+            self._print_tree(self.root)
+    
+    def _print_tree(self, curr_node):
+        if curr_node is not None:
+            self._print_tree(curr_node.left_child)
+            print(curr_node.value)
+            self._print_tree(curr_node.right_child)
+    
+def fill_tree(tree, num_elems=20, max_int=100):
+    from random import randint
+    for _ in range(num_elems):
+        cur_elem=randint(0,max_int)
+        tree.insert(cur_elem)
+    return tree
+
+tree=BinaryTree()
+tree=fill_tree(tree)
+tree.print_tree()
